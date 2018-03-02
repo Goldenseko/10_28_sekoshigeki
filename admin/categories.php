798e1a04d21cd91a56d7cc7b5305de9758206b41
Goldenseko <sekomarcoshigeki@gmail.com>
2018-03-02 09:59:52 +0900

    ファンクションファイルを追加

--- admin/categories.php
+++ admin/categories.php
@@ -19,32 +19,7 @@
 
                     <div class="col-xs-6">
 
-                      <?php
-
-                      if(isset($_POST['submit'])){
-
-                      $cat_title = $_POST['cat_title'];
-
-                      if($cat_title == "" || empty($cat_title)){
-
-                        echo "This field should not be empty";
-
-                      }else{
-
-                        $query = "INSERT INTO categories(cat_title) ";
-                        $query .= "VALUE('{$cat_title}') ";
-
-                        $create_category_query = mysqli_query($connection, $query);
-
-                        if(!$create_category_query){
-
-                          die('QUERY FAILED' . mysqli_error($connection));
-                        }
-
-                      }
-                    }
-
-                      ?>
+                      <?php insert_categories();  ?>
 
 
                       <form class="" action="" method="post">
@@ -58,15 +33,20 @@
                           <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                         </div>
                       </form>
-                    </div>
+
+                      <?php
+                      if(isset($_GET['edit'])){
+
+                        $cat_id = $_GET['edit'];
+
+                        include "includes/update_categories.php";
+
+
+                      }
+                      ?>
+                    </div> //Add Edit Category Form
+
                     <div class="col-xs-6">
-
-                  <?php
-                  $query = "SELECT * FROM categories LIMIT 3";
-                  $select_categories = mysqli_query($connection,$query);
-                  ?>
-
-
 
                         <table class="table table-bordered table-hover">
                           <thead>
@@ -76,16 +56,36 @@
                           </tr>
                           </thead>
                           <tbody>
-                  <?php
+                  <?php  //Finding All categories Query
+                  $query = "SELECT * FROM categories LIMIT 20";
+                  $select_categories = mysqli_query($connection,$query);
+
                   while($row = mysqli_fetch_assoc($select_categories)){
                   $cat_id = $row['cat_id'];
                   $cat_title = $row['cat_title'];
                   echo "<tr>";
                   echo "<td>{$cat_id}</td>";
                   echo "<td>{$cat_title}</td>";
+                  echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                  echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
                   echo "</tr>";
                   }
                   ?>
+
+                  <?php  //DELETE QUERY
+                  if(isset($_GET['delete'])){
+
+                    $the_cat_id = $_GET['delete'];
+
+                    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+                    $delete_query = mysqli_query($connection,$query);
+
+                    header("Location: categories.php");
+                  }
+
+
+
+                   ?>
 
 
                             <tr>
