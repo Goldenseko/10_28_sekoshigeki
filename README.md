677a92bed0ceb8f26c8b2096924d59b122044824
Goldenseko <sekomarcoshigeki@gmail.com>
2018-03-02 21:12:02 +0900

    admin内にposts.phpを追加

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 # 3月1日、初回push, 未完成な部分も多く、データベースの文字化けが解決できていないが素材を一度Git_Hub上に提出。引き続き原因究明。
 
 # admin_navigation.phpのcategoryのリンクからcategories.phpに飛ぶようにリンクを修正
+
+# posts.phpを作成。postsを新たに追加するページを作成。
--- admin/categories.php
+++ admin/categories.php
@@ -34,7 +34,7 @@
                         </div>
                       </form>
 
-                      <?php
+                      <?php //UPDATE and INCLUDE
                       if(isset($_GET['edit'])){
 
                         $cat_id = $_GET['edit'];
@@ -56,42 +56,14 @@
                           </tr>
                           </thead>
                           <tbody>
-                  <?php  //Finding All categories Query
-                  $query = "SELECT * FROM categories LIMIT 20";
-                  $select_categories = mysqli_query($connection,$query);
 
-                  while($row = mysqli_fetch_assoc($select_categories)){
-                  $cat_id = $row['cat_id'];
-                  $cat_title = $row['cat_title'];
-                  echo "<tr>";
-                  echo "<td>{$cat_id}</td>";
-                  echo "<td>{$cat_title}</td>";
-                  echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                  echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                  echo "</tr>";
-                  }
-                  ?>
+                  <?php  findAllCategories();  ?>
 
-                  <?php  //DELETE QUERY
-                  if(isset($_GET['delete'])){
 
-                    $the_cat_id = $_GET['delete'];
-
-                    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
-                    $delete_query = mysqli_query($connection,$query);
-
-                    header("Location: categories.php");
-                  }
+                  <?php deleteCategories(); ?>
 
 
 
-                   ?>
-
-
-                            <tr>
-                              <td>BaseBall Category</td>
-                              <td>Basket Ball Category</td>
-                            </tr>
                           </tbody>
                         </table>
 
