7dc156682d2ccd4f67614ebf14abb0a24330cd1c
Goldenseko <sekomarcoshigeki@gmail.com>
2018-03-02 21:18:21 +0900

    posts.phpを編集

--- admin/posts.php
+++ admin/posts.php
@@ -13,7 +13,7 @@
                     <div class="col-lg-12">
 
                       <h1 class="page-header">
-                          Welcome to Posts
+                          Welcome to Posts を編集！
                           <small>Author : Shigeki Seko and Others!</small>
                       </h1>
 
